b6c6406faf7b06cc152842e5765031d0e3b6772e
chenxm <chen_xm@sjtu.edu.cn>
2012-10-18 21:22:57 +0800

    Parse canlander

--- archive_crawler.py
+++ archive_crawler.py
@@ -11,12 +11,12 @@ from html5lib import treebuilders
 wayback_search_prefix = 'http://wayback.archive.org/web/'
 #***********************************
 
-parser = argparse.ArgumentParser(description="Parse the historical\
-version URLs of a web page in Weyback (http://wayback.archive.org/).\
-")
-parser.add_argument("pagelist", type=str, help="A plain file containing\
-the addresses of web pages.")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description="Parse the historical\
+# version URLs of a web page in Weyback (http://wayback.archive.org/).\
+# ")
+# parser.add_argument("pagelist", type=str, help="A plain file containing\
+# the addresses of web pages.")
+# args = parser.parse_args()
 
 def open_url(url):
 	try:
@@ -30,8 +30,36 @@ def open_url(url):
 		res = None
 	return res
 
-html_doc = open_url("http://www.baidu.com")
-if html_doc != None:
+def parse_canlendar(wbcal):
+	""" Parse teh calendar toolkit
+	@wbcal: the calendar division, which identified by id="wbCalendar"
+	"""
+	if not etree.iselement(wbcal):
+		print 'Not Element object.'
+		return []
+	calOver = wbcal.find("./{*}div[@id='calOver']")
+	for child in calOver.getchildren():
+		print child.attrib['id']
+
+#html_doc = open_url(wayback_search_prefix + '*/www.sina.com.cn')
+wholepage = ''
+if wholepage != None:
 	parser = html5lib.HTMLParser(tree = treebuilders.getTreeBuilder("lxml"))
-	etree_doc = parser.parse(html_doc)
-	print etree.tostring(etree_doc)
+	html_doc = parser.parse(open("sinares"))	# offline file for test
+	body = html_doc.find("./{*}body")
+	position_div = body.find("./{*}div[@id='position']")
+	wayback_sketch = position_div.find("./{*}div[@id='wbSearch']/{*}div[@id='form']/{*}div[@id='wbMeta']/{*}p[@class='wbThis']")
+	wayback_url = wayback_sketch.getchildren()[-1].attrib['href']
+	wayback_cal = position_div.find("./{*}div[@id='wbCalendar']")
+	# Parse the calendar
+	parse_canlendar(wayback_cal)
+
+
+
+
+
+
+
+
+
+
